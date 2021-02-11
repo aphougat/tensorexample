d75f3ee5f8a7b72bc00b077eb3abee0e924a1659
abhayphougat <abhayphougat@gmail.com>
2021-02-11 10:23:46 +0530

    reading 311 call data and generating frequency

---
+++ 311calls.py
@@ -0,0 +1,24 @@
+import csv
+import time
+
+
+def read(filename, date_idx, date_parse, year, bucket=7):
+    days_in_yr = 365
+    freq = {}
+    for period in range(0, int(days_in_yr/bucket)):
+        freq[period] = 0
+    with open(filename, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            if row[date_idx] == '' or reader.line_num == 1:
+                continue
+            else:
+                t = time.strptime(row[date_idx], date_parse)
+                if t.tm_year == year and t.tm_yday < (days_in_yr - 1):
+                    freq[int(t.tm_yday/bucket)] += 1
+    return freq
+
+
+freq = read("/Users/abhayphougat/Downloads/311.csv", 1, '%m/%d/%Y %H:%M:%S %p', 2014)
+
+print(freq)
